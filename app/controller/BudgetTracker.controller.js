2654a9adc681e0c08f3345b3311dee743b04fcd3
Sean Cali <seankenjicali@gmail.com>
2017-03-13 11:50:13 -0700

    Final Version

--- app/controller/BudgetTracker.controller.js
+++ app/controller/BudgetTracker.controller.js
@@ -3,20 +3,23 @@
 
     angular
         .module('BudgetTracker')
-        .controller('BudgetTrackerCtrl', btCtrl);
+        .controller('BudgetTrackerCtrl', BudgetTrackerCtrl);
 
     /* @ngInject */
-    function btCtrl() {
+    function BudgetTrackerCtrl() {
         var vm = this;
 
+        // Declare empty expense and income arrays
         vm.addExpense = [];
         vm.addIncome = [];
 
+        // Function to add another item to the expense array when clicked
         vm.addExpenseClick = function (name, amount) {
           vm.addExpense.push(vm.expenseInput);
           vm.expenseInput = {};
         };
 
+        // Function to add another item to the income array when clicked
         vm.addIncomeClick = function (name, amount) {
           vm.addIncome.push(vm.incomeInput);
           vm.incomeInput = {};
